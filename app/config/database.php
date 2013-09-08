0aec58689e55df7a0967e48cf904bf717f67b4b3
Zack Davis <zack@2721west.com>
2013-09-08 17:49:04 -0500

    18

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'tunnel.pagodabox.com:3306',
-			'database'  => 'aiga-app-db',
-			'username'  => 'sau',
-			'password'  => '87tTVDlW',
+			'host'      => $_SERVER["DB1_HOST"],
+			'database'  => $_SERVER["DB1_NAME"],
+			'username'  => $_SERVER["DB1_USER"],
+			'password'  => $_SERVER["DB1_PASS"],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
